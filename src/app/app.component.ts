d7fb50d74f906c92c77806ccfeeadf3e921f4b4e
Lucas <lucas.poma.t@gmail.com>
2021-04-18 22:46:06 -0500

    1er commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quiz';
+  name =  "Gian";
+  textPlaceHolder="Escribe tu nombre aquí";
+  disabledPlaceHolder=true;
+  srcImg = "https://www.xtrafondos.com/descargar.php?id=3183&resolucion=3840x1633";
+  texto = "Esto es un ejemplo de Event Binding";
+  texto2 = "";
+  widthX = "";
+  heightY = "";
+  mostrar=true;
+
+  listadoAlumnos: any[] = [
+    {nombre:'Lucas2', estado:'En curso'},
+    {nombre:'Lucas3', estado:'Aprobado'},
+    {nombre:'Lucas4', estado:'Aprobado'}
+  ];
+
+
+
+  constructor(){
+  setInterval( () =>this.name = "Lucas",3000); 
+  setInterval( () =>this.disabledPlaceHolder=false,3000);
+  
+  }
+
+   cambiarTexto(){
+    this.texto = "Cambio";
+  }
+
+  getSuma(num1: number, num2: number){
+    return num1 + num2;
+  }
+
+  toggle(){
+    this.mostrar = !this.mostrar;
+  }
+
+
 }
+
